f7425962a357f98b1bae714c1b63f2d711a356d1
rickypazd <rickypazd@hotmail.com>
2023-06-20 14:53:16 -0400

    Commit first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native'
+const App = (props) => {
+    return <View style={{
+        width:"100%",
+        flex:1,
+        backgroundColor:"#fff"
+
+    }}>
+
+    </View>
+}
+export default App;
